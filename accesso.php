088bdf04b81a4b8d85eec330a55d248dc4956167
Simone <s.grimaldi33@studenti.unisa.it>
2025-01-14 23:32:10 +0100

    aggiutna icon

--- accesso.php
+++ accesso.php
@@ -78,9 +78,12 @@ function insert_utente($username, $password, $nome, $cognome) {
 <html lang="it">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="./img/icon.png">
+    <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styleAccesso1.css">
-    <title>Login e Registrazione AJAX</title>
+    <title>Pagina di accesso</title>
 </head>
 <body>
     <header>
