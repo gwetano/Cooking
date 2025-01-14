9c1d4cf79894ff3a946887a07766c719382bd31a
Simone <s.grimaldi33@studenti.unisa.it>
2025-01-14 20:41:53 +0100

    css casuale

--- accesso.php
+++ accesso.php
@@ -93,9 +93,6 @@ function insert_utente($username, $password, $nome, $cognome) {
             <input type="text" name="username" class="username" placeholder="Username" required> <br>
             <input type="password" name="password" class="password" placeholder="Password" required><br>
             <p>
-                <input type="checkbox" name="ricordami" class="checkbox"> Ricordami
-            </p>
-            <p>
                 <button type="submit">Accedi</button>
             </p>
             <p>Non hai un account? <span id="registratiCliccabile" class="registratiCliccabile">Registrati</span></p>
@@ -105,7 +102,7 @@ function insert_utente($username, $password, $nome, $cognome) {
     <script>
         document.getElementById('loginForm').addEventListener('submit', function(event) {
             event.preventDefault();
-            const formData = new FormData(this);
+            const formData = new FormData(this); 
             fetch('?action=login', {
                 method: 'POST',
                 body: formData
@@ -124,11 +121,11 @@ function insert_utente($username, $password, $nome, $cognome) {
         document.getElementById('registratiCliccabile').addEventListener('click', function() {
             document.getElementById('main-content').innerHTML = `
                 <form id="registerForm">
-                    <input type="text" name="nome" placeholder="Nome" required><br>
-                    <input type="text" name="cognome" placeholder="Cognome" required><br>
-                    <input type="text" name="usernameRegistrazione" placeholder="Username" required><br>
-                    <input type="password" name="passwordRegistrazione" placeholder="Password" required><br>
-                    <input type="password" name="repassword" placeholder="Ripeti Password" required><br>
+                    <input type="text" name="nome" placeholder="Nome" required class="nome"><br>
+                    <input type="text" name="cognome" placeholder="Cognome" required class="cognome"><br>
+                    <input type="text" name="usernameRegistrazione" placeholder="Username" required class="username"><br>
+                    <input type="password" name="passwordRegistrazione" placeholder="Password" required class="password"><br>
+                    <input type="password" name="repassword" placeholder="Ripeti Password" required class="ripetipassword"><br>
                     <p>
                         <input type="checkbox" name="terms" required> Accetto termini e condizioni
                     </p>
