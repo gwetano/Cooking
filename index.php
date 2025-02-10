fd3136a2fe4478874887259dfec340e1e48259ae
simonegrimaldi <simonegrimaldi84@gmail.com>
2025-02-10 17:04:04 +0100

    Index.php + syle TERMINATA

--- index.php
+++ index.php
@@ -52,14 +52,16 @@ function getRicettePreferite($username)
     return [];
 }
 
-function addRicettePreferite($username,$id) {
+function addRicettePreferite($username, $id)
+{
     global $db;
-    $sql = "INSERT INTO preferiti(username, id) VALUES($1, $2";
+    $sql = "INSERT INTO preferiti(username, id) VALUES($1, $2)";
     $result = pg_query_params($db, $sql, array($username, $id));
     return $result !== false;
 }
 
-function removeRicettePreferite($username, $id) {
+function removeRicettePreferite($username, $id)
+{
     global $db;
     $sql = "DELETE FROM preferiti WHERE id = $1 AND username = $2";
     $result = pg_query_params($db, $sql, array($id, $username));
@@ -74,18 +76,20 @@ function removeRicettePreferite($username, $id) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="allStyle.css">
     <title>INDEX</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="icon" href="./img/icon.png">
-    <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap">
 
 </head>
 
-<body>
-    <header>
+<body class="index">
+
+    <header class="index">
         <div class="logo">
             <img src="./img/icon.png" height="50px" width="50px">
         </div>
@@ -94,135 +98,68 @@ function removeRicettePreferite($username, $id) {
             <h1>Cooking</h1>
         </div>
         <?php if (isset($_SESSION['username'])) { ?>
-            <div class="account1">
+            <div class="loggato">
                 <a href="./account.php"> <img src="./img/username.png" height="50px" width="50px"></a>
             </div>
         <?php } else { ?>
-            <div class="account2">
+            <div class="nonLoggato">
                 <a href="./accesso.php"> Accesso</a>
             </div>
         <?php } ?>
     </header>
-    <main>
+
+    <main class="index">
+
         <?php if (isset($_SESSION['username'])) {
+/* UTENTE LOGGATO */
             $username = $_SESSION['username']; ?>
             <div class="mainColumn1">
+                <div class="contenutoMainColumn">
                     <h1>Le tue ricette preferite</h1>
-                    <div>
-                        <?php
-                        $ricettePreferite = getRicettePreferite($username);
-                        if (!empty($ricettePreferite)) {
+                    <?php
+                    $ricettePreferite = getRicettePreferite($username);
+                    if (!empty($ricettePreferite)) { ?>
+                        <div class="ricette">
+                            <?php
                             foreach ($ricettePreferite as $id) {
                                 $nome = getNomeRicetta($id);
                                 $descrizione = getDescrizioneRicetta($id);
                                 $foto = getFotoRicetta($id);
                                 ?>
-                                <div class="ricetta">
-                                    <h2>
+                                <div class="ricetta<?php echo $id ?>" onclick="vaiAllaRicetta(event, <?php echo $id; ?>)">
+                                    <div class="nomeRicetta">
                                         <?php
                                         echo getNomeRicetta($id); ?>
-                                    </h2>
-                                    <p>
+                                    </div>
+                                    <div class="descrizioneRicetta">
                                         <?php
                                         echo getDescrizioneRicetta($id) ?>
-                                    </p>
-                                        <img src="<?php echo getFotoRicetta($id); ?>" alt="" height="50px" width="50px">
+                                    </div>
+                                    <div class="classFotoRicetta">
+                                        <img src="<?php echo htmlspecialchars(getFotoRicetta($id)); ?>" alt="ricetta1"
+                                            height="100%">
+                                    </div>
                                 </div>
+
                                 <?php
                             }
-                        } else {
-                            echo "Non ci sono ricette preferite per l'utente.";
-                        }
-                        ?>
-                    </div>
+                            ?>
+                        </div><?php
+                    } else { ?>
+                        <div class="contenutoEmptyMainColumn1">
+                            <p>Nessuna ricetta nei preferiti</p>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                </div>
             </div>
+
             <div class="mainColumn2">
-                <h1>Le più cliccate</h1>
-                <div class="ricette">
-                    <div class="ricetta1" onclick="vaiAllaRicetta(1)">
-                        <div class="nomeRicetta">
-                            <?php
-                            echo getNomeRicetta(1); ?>
-                        </div>
-                        <div class="descrizioneRicetta">
-                            <?php
-                            echo getDescrizioneRicetta(1) ?>
-                        </div>
-                        <div class="classFotoRicetta">
-                            <img src=<?php
-                            echo getFotoRicetta(1) ?> alt="ricetta1" height="100%">
-                        </div>
-                    </div>
-
-                    <div class="ricetta2" onclick="vaiAllaRicetta(2)">
-                        <div class="nomeRicetta">
-                            <?php
-                            echo getNomeRicetta(2); ?>
-                        </div>
-                        <div class="descrizioneRicetta">
-                            <?php
-                            echo getDescrizioneRicetta(2) ?>
-                        </div>
-                        <div class="classFotoRicetta">
-                            <img src=<?php
-                            echo getFotoRicetta(2) ?> alt="ricetta2" height="100%">
-                        </div>
-                    </div>
-
-                    <div class="ricetta3" onclick="vaiAllaRicetta(3)">
-                        <div class="nomeRicetta">
-                            <?php
-                            echo getNomeRicetta(3); ?>
-                        </div>
-                        <div class="descrizioneRicetta">
-                            <?php
-                            echo getDescrizioneRicetta(3) ?>
-                        </div>
-                        <div class="classFotoRicetta">
-                            <img src=<?php
-                            echo getFotoRicetta(3) ?> alt="ricetta3" height="100%">
-                        </div>
-                    </div>
-
-                    <div class="ricetta4" onclick="vaiAllaRicetta(4)">
-                        <div class="nomeRicetta">
-                            <?php
-                            echo getNomeRicetta(4); ?>
-                        </div>
-                        <div class="descrizioneRicetta">
-                            <?php
-                            echo getDescrizioneRicetta(4) ?>
-                        </div>
-                        <div class="classFotoRicetta">
-                            <img src=<?php
-                            echo getFotoRicetta(4) ?> alt="ricetta4" height="100%">
-                        </div>
-                    </div>
-                </div>
-                <div class="all">
-                    <a href="./home.php">Vai a tutte le ricette ▶</a>
-                </div>
-            </div>
-                <?php
-        } else { ?>
-                <div class="mainColumn1">
-                    <h2>
-                        <h1>Ricetta del giorno</h1>
-                        <h2>
-                                <?php
-                                echo getNomeRicetta(4); ?>
-                            </h2>
-                        <p>
-                                <?php
-                                echo getDescrizioneRicetta(4) ?>
-                        </p>
-                            <img src=<?php echo getFotoRicetta(4) ?> alt="ricetta4" width="30%">
-                    </h2>
-                </div>
-                <div class="mainColumn2">
+                <div class="contenutoMainColumn">
                     <h1>Le più cliccate</h1>
                     <div class="ricette">
-                        <div class="ricetta1" onclick="vaiAllaRicetta(1)">
+                        <div class="ricetta1" onclick="vaiAllaRicetta(event,1)">
                             <div class="nomeRicetta">
                                 <?php
                                 echo getNomeRicetta(1); ?>
@@ -237,7 +174,7 @@ function removeRicettePreferite($username, $id) {
                             </div>
                         </div>
 
-                        <div class="ricetta2" onclick="vaiAllaRicetta(2)">
+                        <div class="ricetta2" onclick="vaiAllaRicetta(event,2)">
                             <div class="nomeRicetta">
                                 <?php
                                 echo getNomeRicetta(2); ?>
@@ -252,7 +189,7 @@ function removeRicettePreferite($username, $id) {
                             </div>
                         </div>
 
-                        <div class="ricetta3" onclick="vaiAllaRicetta(3)">
+                        <div class="ricetta3" onclick="vaiAllaRicetta(event,3)">
                             <div class="nomeRicetta">
                                 <?php
                                 echo getNomeRicetta(3); ?>
@@ -267,7 +204,7 @@ function removeRicettePreferite($username, $id) {
                             </div>
                         </div>
 
-                        <div class="ricetta4" onclick="vaiAllaRicetta(4)">
+                        <div class="ricetta4" onclick="vaiAllaRicetta(event,4)">
                             <div class="nomeRicetta">
                                 <?php
                                 echo getNomeRicetta(4); ?>
@@ -282,17 +219,109 @@ function removeRicettePreferite($username, $id) {
                             </div>
                         </div>
                     </div>
-                    <div class="all">
+                    <div class="goHome">
                         <a href="./home.php">Vai a tutte le ricette ▶</a>
                     </div>
                 </div>
-                <?php
+            </div>
+            <?php
+/* UTENTE NON LOGGATO */
+        } else { ?>
+            <div class="mainColumn1">
+                <div class="contenutoMainColumn">  
+                    <h1>Ricetta del giorno</h1>
+                    <div class="ricette" id="ricettaDelGiorno">
+                        <div class="ricetta1">
+                            <div class="nomeRicetta">
+                                <?php
+                                echo getNomeRicetta(1); ?>
+                            </div>
+                            <div class="descrizioneRicetta">
+                                <?php
+                                echo getDescrizioneRicetta(1) ?>
+                            </div>
+                            <div class="classFotoRicetta">
+                                <img src=<?php
+                                echo getFotoRicetta(1) ?> alt="ricetta1" height="100%">
+                            </div>
+                        </div>
+                    </div>
+            </div>
+
+            </div>
+            <div class="mainColumn2">
+                <div class="contenutoMainColumn">
+                <h1>Le più cliccate</h1>
+                <div class="ricette">
+                    <div class="ricetta1">
+                        <div class="nomeRicetta">
+                            <?php
+                            echo getNomeRicetta(1); ?>
+                        </div>
+                        <div class="descrizioneRicetta">
+                            <?php
+                            echo getDescrizioneRicetta(1) ?>
+                        </div>
+                        <div class="classFotoRicetta">
+                            <img src=<?php
+                            echo getFotoRicetta(1) ?> alt="ricetta1" height="100%">
+                        </div>
+                    </div>
+
+                    <div class="ricetta2">
+                        <div class="nomeRicetta">
+                            <?php
+                            echo getNomeRicetta(2); ?>
+                        </div>
+                        <div class="descrizioneRicetta">
+                            <?php
+                            echo getDescrizioneRicetta(2) ?>
+                        </div>
+                        <div class="classFotoRicetta">
+                            <img src=<?php
+                            echo getFotoRicetta(2) ?> alt="ricetta2" height="100%">
+                        </div>
+                    </div>
+
+                    <div class="ricetta3">
+                        <div class="nomeRicetta">
+                            <?php
+                            echo getNomeRicetta(3); ?>
+                        </div>
+                        <div class="descrizioneRicetta">
+                            <?php
+                            echo getDescrizioneRicetta(3) ?>
+                        </div>
+                        <div class="classFotoRicetta">
+                            <img src=<?php
+                            echo getFotoRicetta(3) ?> alt="ricetta3" height="100%">
+                        </div>
+                    </div>
+
+                    <div class="ricetta4">
+                        <div class="nomeRicetta">
+                            <?php
+                            echo getNomeRicetta(4); ?>
+                        </div>
+                        <div class="descrizioneRicetta">
+                            <?php
+                            echo getDescrizioneRicetta(4) ?>
+                        </div>
+                        <div class="classFotoRicetta">
+                            <img src=<?php
+                            echo getFotoRicetta(4) ?> alt="ricetta4" height="100%">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <?php
         }
         ?>
 
     </main>
     <footer>
-        <p>
+        <div>
             <a href="">
                 Privacy Policy
             </a>
@@ -309,7 +338,7 @@ function removeRicettePreferite($username, $id) {
                 Welcome
             </a>
             ● © 2025 Cooking
-        </p>
+        </div>
     </footer>
 </body>
 
@@ -317,12 +346,28 @@ function removeRicettePreferite($username, $id) {
 
 <script>
 
-function vaiAllaRicetta(id) {
+    function vaiAllaRicetta(event, id) {
         // Cambia la pagina in base all'ID della ricetta
+        event.stopPropagation();
         window.location.href = 'ricetta.php?id=' + id;
     }
 
-    function favorites(event,id, toggle) {
+    document.addEventListener('DOMContentLoaded', function () {
+    const ricette = document.querySelectorAll('.ricetta1, .ricetta2, .ricetta3, .ricetta4');
+    const isLoggedIn = <?php echo isset($_SESSION['username']) ? 'true' : 'false'; ?>;
+
+    ricette.forEach(ricetta => {
+        if (isLoggedIn) {
+            ricetta.classList.add('cursor-pointer');
+            ricetta.classList.remove('cursor-not-allowed');
+        } else {
+            ricetta.classList.add('cursor-not-allowed');
+            ricetta.classList.remove('cursor-pointer');
+        }
+    });
+    });
+
+    function favorites(event, id, toggle) {
         event.stopPropagation();
         const star = document.getElementById('addPreferiti' + id);
         fetch('home.php', {
