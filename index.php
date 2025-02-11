a4f926e6767058da6c16b7eaaf68c4e8ddf90d6b
simonegrimaldi <simonegrimaldi84@gmail.com>
2025-02-11 10:41:20 +0100

    css generale

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ function removeRicettePreferite($username, $id)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="allStyle.css">
+    <link rel="stylesheet" href="allStyle1.css">
     <title>INDEX</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -337,7 +337,12 @@ function removeRicettePreferite($username, $id)
             <a href="./index.php">
                 Welcome
             </a>
-            ● © 2025 Cooking
+            <a>
+                ●
+            </a>
+            <a href="#">
+                © 2025 Cooking
+            </a>
         </div>
     </footer>
 </body>
