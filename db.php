faae00dd993c42f375d098685b8bc66584ff0d73
simonegrimaldi <simonegrimaldi84@gmail.com>
2025-02-19 11:04:46 +0100

    general resolution

--- db.php
+++ db.php
@@ -6,6 +6,5 @@ $username = 'www';
 $password = 'tw2024';
 $connection_string = "host=$host dbname=$db port=$port user=$username password=$password";
 
-//CONNESSIONE AL DB
 $db = pg_connect($connection_string) or die('Impossibile connetersi al database: ' . pg_last_error());
 ?>
