b05308e9e628072a03ae4d5c68f91d63ee1f3331
Simone <s.grimaldi33@studenti.unisa.it>
2025-02-20 16:50:18 +0100

    change logout re-direction

--- account.php
+++ account.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['username'])) {
 if (isset($_GET['logout'])) {
     session_unset();
     session_destroy();
-    header("Location: accesso.php");
+    header("Location: index.php");
     exit;
 }
 
